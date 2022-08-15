424073f071a6587cfe8ec3467c793b92014778ef
CrypMetis <104890916+CrypMetis@users.noreply.github.com>
2022-08-15 09:40:34 +0800

    Update IERC20.sol

--- contracts/interfaces/IERC20.sol
+++ contracts/interfaces/IERC20.sol
@@ -8,9 +8,7 @@ interface IERC20 {
 
     function transfer(address recipient, uint256 amount) external returns (bool);
 
-
     function allowance(address owner, address spender) external view returns (uint256);
-
 
     function approve(address spender, uint256 amount) external returns (bool);
 
