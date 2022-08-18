e16fe5b255edee664d4600305b916004dcfabf2a
CrypMetis <104890916+CrypMetis@users.noreply.github.com>
2022-08-18 09:46:03 +0800

    Update IERC20.sol

--- contracts/interfaces/IERC20.sol
+++ contracts/interfaces/IERC20.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.8.0;
 interface IERC20 {
+
     function totalSupply() external view returns (uint256);
 
     function balanceOf(address account) external view returns (uint256);
