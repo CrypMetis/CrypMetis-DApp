3212a4e29b2a203474dd412e0fd9cc676a87fe51
CrypMetis <104890916+CrypMetis@users.noreply.github.com>
2022-08-20 09:19:40 +0800

    Update TransferHelper.sol

--- contracts/lib/TransferHelper.sol
+++ contracts/lib/TransferHelper.sol
@@ -1,6 +1,4 @@
 pragma solidity ^0.8.0;
-
-
 // helper methods for interacting with ERC20 tokens and sending ETH that do not consistently return true/false
 library TransferHelper {
     function safeApprove(address token, address to, uint value) internal {
