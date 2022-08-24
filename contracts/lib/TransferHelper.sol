c171c153b383540d317ecbff9fa40fb2070121fc
CrypMetis <104890916+CrypMetis@users.noreply.github.com>
2022-08-24 09:18:26 +0800

    Update TransferHelper.sol

--- contracts/lib/TransferHelper.sol
+++ contracts/lib/TransferHelper.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.8.0;
+
 library TransferHelper {
     function safeApprove(address token, address to, uint value) internal {
         // bytes4(keccak256(bytes('approve(address,uint256)')));
